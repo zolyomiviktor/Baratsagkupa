444335e961bfb0ca41e435334e70419d97e9620c
zolyomiviktor <72090821+zolyomiviktor@users.noreply.github.com>
2021-09-23 10:19:09 +0200

    Delete Barátság Kupa directory

--- Barátság Kupa/nev.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nev=$_REQUEST['nev'];
-$parancs="INSERT INTO nevezok (nev) VALUES ('$nev');";
-$kapcsolat=mysqli_connect("127.0.0.1","root","","baratsagkupa");
-mysqli_query($kapcsolat, $parancs);
-mysqli_close($kapcsolat);
-header("Location:index.html");
-?>
