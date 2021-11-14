faff70b967a3aef2edc7128e3c2da487c9227f03
zolyomiviktor <72090821+zolyomiviktor@users.noreply.github.com>
2021-11-14 21:44:53 +0100

    Add files via upload
    2021.11.14.

--- fogas.php
+++ fogas.php
@@ -1,12 +1,14 @@
 <?php
+$tomege=$_POST['tomege'];
+$helyszama=$_POST['helyszama'];
+$darabszama=$_POST['darabszama'];
+$kategoriaja=$_POST['kategoriaja'];
 $kapcsolat=mysqli_connect("127.0.0.1","root","","baratsagkupa");
-foreach($_POST as $key => $value){
-    $$key=$value;
-} 
-$parancs="INSERT INTO fogasnaplo (tomeg,helyszam,darabszam,kategoria ) VALUES ('$tomeg','$helyszam','$darabszam','$kategoria');"; 
+if (!$kapcsolat) {
+    die('Kapcsolódási hiba (' . mysqli_connect_errno() . ') ' . mysqli_connect_error());
+}
+$parancs="INSERT INTO fogasnaplo (tomege,helyszama,darabszama,kategoriaja ) VALUES ('$tomege','$helyszama','$darabszama','$kategoriaja');"; 
 mysqli_query($kapcsolat, $parancs);
 
 mysqli_close($kapcsolat); 
-
-//header("Location:nevezok.php"); 
 ?>
